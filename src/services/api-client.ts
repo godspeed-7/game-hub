ece788de529022a99e9582fd7529fcb0f58812ca
Amnok <ankitsingh80741@gmail.com>
2023-05-20 18:34:18 +0530

    added games API with gamegrid component

---
+++ src/services/api-client.ts
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+const api = axios.create({
+    baseURL: 'https://api.rawg.io/api',
+    params: {
+        key: '0380b720f7c34ffb9673aa3bb4d21268'
+    }
+})
+
+export default api;
