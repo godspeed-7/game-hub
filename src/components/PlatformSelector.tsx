43e8fe9edc3a402167bbb4028981cac06c92345c
Amnok <ankitsingh80741@gmail.com>
2023-06-06 15:31:21 +0530

    refractor duplicate interface

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -2,7 +2,7 @@ import { Button, Menu, MenuButton, MenuItem, MenuList } from '@chakra-ui/react';
 import React from 'react';
 import { BsChevronDown } from 'react-icons/bs';
 import useplatforms from '../hooks/usePlatforms';
-import { Platform } from '../hooks/useGames';
+import { Platform } from '../hooks/usePlatforms';
 
 interface Props {
   onSelectplatform: (platform: Platform) => void;
--- src/components/SortSelector.tsx
+++ src/components/SortSelector.tsx
@@ -1,7 +1,6 @@
 import { Button, Menu, MenuButton, MenuItem, MenuList } from '@chakra-ui/react';
 import { BsChevronDown } from 'react-icons/bs';
 import useplatforms from '../hooks/usePlatforms';
-import { Platform } from '../hooks/useGames';
 
 interface Props {
   onSelectSortOrder: (sortOrder: string) => void;
